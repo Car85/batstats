fee1696adf710dee3a83fb1cdecb157ee7c89d57
Car85 <car85@startmail.com>
2024-12-30 02:34:57 -0800

    checkpoint 6.2: Back and Front Testing.

--- frontend/bats-dashboar-tp/src/pages/App.test.tsx
+++ frontend/bats-dashboar-tp/src/pages/App.test.tsx
@@ -2,7 +2,7 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
 import App from './App';
 
-// Mock de react-papaparse
+// Mock react-papaparse
 vi.mock('react-papaparse', () => ({
   useCSVReader: () => ({
     CSVReader: ({ onUploadAccepted }: any) => (
@@ -26,13 +26,10 @@ describe('App Component - Render', () => {
   it('renders subcomponents after CSV upload', async () => {
     render(<App />);
 
-    // Encuentra el componente mockeado
     const mockCSVReader = screen.getByText(/Mocked CSV Reader/i);
 
-    // Simula el clic para cargar el archivo
     fireEvent.click(mockCSVReader);
 
-    // Verifica que los subcomponentes se rendericen correctamente
     await waitFor(() => {
       expect(screen.getByText(/Your Reports/i)).not.toBeNull();
       expect(screen.getByRole('button', { name: /Save Report/i })).not.toBeNull();
