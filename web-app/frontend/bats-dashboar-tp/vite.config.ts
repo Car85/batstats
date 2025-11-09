501d32a766ca92d1a77f63453dec273c8ab84495
Car85 <car85@startmail.com>
2025-11-09 07:00:17 -0800

    add tailwindcss framework

--- web-app/frontend/bats-dashboar-tp/vite.config.ts
+++ web-app/frontend/bats-dashboar-tp/vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tauri from "vite-plugin-tauri";
+import postcss from "postcss";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     tauri({
       // Opciones opcionales:
       // outDir: 'dist',          // carpeta donde Vite escribe los assets (por defecto "dist")
       // binaryName: 'batstats'   // nombre del binario (por defecto = package.name)
     })
   ],
+ css: { postcss },
  clearScreen: false,
  server: {
    port: 5173,
